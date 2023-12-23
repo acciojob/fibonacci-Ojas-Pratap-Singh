4d1cd7c29bb7e71297d6c40585c520d642a6a227
Ojas Pratap Singh <128834924+Ojas-Pratap-Singh@users.noreply.github.com>
2023-12-23 12:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	let a = 0;
+	let b = 1;
+	if(num==1) return 0 ;
+	if(num==2) return 1;
+	 let c;
+	for(let i =3 ; i<=num ; i++){
+	    c = a + b ;
+		a = b;
+		b = c;
+	}
+	return c ;
 }
 
 module.exports = fibonacci;
